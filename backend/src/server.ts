c8a3adee6d9b1cdbcf62df0e6396e2eb1c5c530a
ismail-888 <67410690+ismail-888@users.noreply.github.com>
2024-02-12 20:40:34 +0200

    finish login and validation part

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,10 @@
 import express from 'express'
 import cors from 'cors'
-import { sample_foods, sample_tags } from './data';
+import { sample_foods, sample_tags, sample_users } from './data';
+import jwt from 'jsonwebtoken'
 
 const app=express();
+app.use(express.json());
 
 // localhost:4200
 // localhost:5000
@@ -11,6 +13,7 @@ app.use(cors({
     origin:['http://localhost:4200']
 }))
 
+// get methods
 app.get('/api/foods',(req,res)=>{
     res.send(sample_foods)
 })
@@ -38,6 +41,30 @@ app.get('/api/foods/:foodId',(req,res)=>{
    const food= sample_foods.find(food=>food.id == foodId);
    res.send(food)
 })
+
+// Post Methods
+app.post("/api/users/login",(req,res)=>{
+    const {email,password}=req.body
+    const user=sample_users.find(user=>user.email===email && user.password === password)
+
+    if(user){
+        res.send(genereateTokenResponse(user))
+    }else{
+        res.status(400).send('User name or password is not valid')
+    }
+})
+
+
+const genereateTokenResponse=(user:any)=>{
+const token=jwt.sign({
+    email:user.email,isAdmin:user.isAdmin
+},"SomeRandomText",{
+    expiresIn:'30d'
+})
+
+user.token=token;
+return user
+}
 
 const port = 5000
 app.listen(port,()=>{
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,3 +1,4 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {HttpClientModule} from '@angular/common/http'
@@ -13,7 +14,14 @@ import { TagsComponent } from './components/partials/tags/tags.component';
 import { CartPageComponent } from './components/pages/cart-page/cart-page.component';
 import { TitleComponent } from './components/partials/title/title.component';
 import { NotFoundComponent } from './components/partials/not-found/not-found.component';
-
+import { LoginPageComponent } from './components/pages/login-page/login-page.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ToastrModule } from 'ngx-toastr';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { InputContainerComponent } from './components/partials/input-container/input-container.component';
+import { InputValidationComponent } from './components/partials/input-validation/input-validation.component';
+import { TextInputComponent } from './components/partials/text-input/text-input.component';
+import { DefaultButtonComponent } from './components/partials/default-button/default-button.component'
 @NgModule({
   declarations: [
     AppComponent,
@@ -24,13 +32,26 @@ import { NotFoundComponent } from './components/partials/not-found/not-found.com
     TagsComponent,
     CartPageComponent,
     TitleComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    LoginPageComponent,
+    InputContainerComponent,
+    InputValidationComponent,
+    TextInputComponent,
+    DefaultButtonComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     // RatingModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule,
+    ToastrModule.forRoot({
+      timeOut:3000,
+      positionClass:'toast-bottom-right',
+      newestOnTop:false
+    }),
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
